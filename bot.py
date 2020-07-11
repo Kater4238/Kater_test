3c478502588e2eee120f60a7c5ff3dc28a448341
Kater4238 <68147944+Kater4238@users.noreply.github.com>
2020-07-11 19:56:39 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(731447956724776980)
+    await channel.send(f'{member} Join!')
+
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(731470362621247489)
+    await channel.send(f'{member} Leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"{round(bot.latency*1000)} ms")
+
+bot.run("NzMxNDQ4Njk0OTUxNjQxMjIw.XwmlTg.pxzGRMeMMtV_2tC9MxneD6Ktw6U")
